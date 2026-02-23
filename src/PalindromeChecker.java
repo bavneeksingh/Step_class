b0bef7ef38f508693599d3e97a692c7f66f70697
bavneeksingh <bavneeksingh2004@gmail.com>
2026-02-23 10:32:21 +0530

    Added new code in U7

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -3,23 +3,29 @@ import java.util.Scanner;
 public class PalindromeChecker{
 
     public static void main(String[] args) {
-        // UC2 - Hardcoded Palindrome
-        // Take ONLY ONE INPUT for all remaining UC
         Scanner scanner=new Scanner(System.in);
         System.out.print("\nEnter a string to check palindrome: ");
         String input = scanner.nextLine();
 
-        // UC5 - Stack Method
-        System.out.println("\nUC5 - Using Stack:");
-        Stack<Character> stack = new Stack<>();
+        // UC6 - Queue + Stack Method
+        System.out.println("\nUC6 - Using Queue + Stack:");
+        Stack<Character> stack2 = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
         for (char c : input.toCharArray()) {
-            stack.push(c);
+            stack2.push(c);
+            queue.add(c);
         }
 
-        String stackReverse = "";
-        while (!stack.isEmpty()) {
-            stackReverse += stack.pop();
+        boolean isPalindromeQS = true;
+        while (!stack2.isEmpty()) {
+            if (!stack2.pop().equals(queue.remove())) {
+                isPalindromeQS = false;
+                break;
+            }
         }
-        System.out.println(input.equals(stackReverse) ? "Palindrome" : "Not Palindrome");
+        System.out.println(isPalindromeQS ? "Palindrome" : "Not Palindrome");
+
+
     }
 }
