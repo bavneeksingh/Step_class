1d13f86a175358c4b2856f103996ed3c8a0cd8ef
bavneeksingh <bavneeksingh2004@gmail.com>
2026-02-23 10:48:25 +0530

    Added/updated code in UC6

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -6,18 +6,24 @@ public class PalindromeChecker{
         Scanner scanner=new Scanner(System.in);
         System.out.print("\nEnter a string to check palindrome: ");
         String input = scanner.nextLine();
-        // UC5 - Stack Method
-        System.out.println("\nUC5 - Using Stack:");
-        Stack<Character> stack = new Stack<>();
+        // UC6 - Queue + Stack Method
+        System.out.println("\nUC6 - Using Queue + Stack:");
+        Stack<Character> stack2 = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
         for (char c : input.toCharArray()) {
-            stack.push(c);
+            stack2.push(c);
+            queue.add(c);
         }
 
-        String stackReverse = "";
-        while (!stack.isEmpty()) {
-            stackReverse += stack.pop();
+        boolean isPalindromeQS = true;
+        while (!stack2.isEmpty()) {
+            if (!stack2.pop().equals(queue.remove())) {
+                isPalindromeQS = false;
+                break;
+            }
         }
-        System.out.println(input.equals(stackReverse) ? "Palindrome" : "Not Palindrome");
+        System.out.println(isPalindromeQS ? "Palindrome" : "Not Palindrome");
 
 
 
