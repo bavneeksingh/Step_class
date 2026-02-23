4c90e007660be70857f6d1a2e76341eb119801cc
bavneeksingh <bavneeksingh2004@gmail.com>
2026-02-23 10:39:26 +0530

    Added/updated code in U3

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -7,24 +7,11 @@ public class PalindromeChecker{
         System.out.print("\nEnter a string to check palindrome: ");
         String input = scanner.nextLine();
 
-        // UC6 - Queue + Stack Method
-        System.out.println("\nUC6 - Using Queue + Stack:");
-        Stack<Character> stack2 = new Stack<>();
-        Queue<Character> queue = new LinkedList<>();
+        // UC3 - String Reverse Method
+        System.out.println("\nUC3 - Using String Reverse:");
+        String reversed = new StringBuilder(input).reverse().toString();
+        System.out.println(input.equals(reversed) ? "Palindrome" : "Not Palindrome");
 
-        for (char c : input.toCharArray()) {
-            stack2.push(c);
-            queue.add(c);
-        }
-
-        boolean isPalindromeQS = true;
-        while (!stack2.isEmpty()) {
-            if (!stack2.pop().equals(queue.remove())) {
-                isPalindromeQS = false;
-                break;
-            }
-        }
-        System.out.println(isPalindromeQS ? "Palindrome" : "Not Palindrome");
 
 
     }
